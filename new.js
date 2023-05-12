1433a4572bd8fb1c10598a0c2465246692a58978
Jakh <numanov94@list.ru>
2023-05-12 19:46:37 +0900

    test done

---
+++ new.js
@@ -0,0 +1,35 @@
+// let str = 'fozil';
+
+// for(let i = str.length; i >= 0; i--){
+//     console.log(str[i]);
+// }
+
+// for(let = 1; i <= 5; i++){
+//    if(i %2 === 0){
+//     console.log(i, 'juft son');   
+//    }else{
+//     console.log(i,'toq son');
+//    }
+
+
+// }
+
+
+
+
+
+
+// let str = 'WebBrainAcadAmya';
+// let count = 0;
+
+// for (let i = 0; i <str.length;i++){
+// if (let [i] === 'a' || str [i] === 'A'){
+//    // newStr += str [i]
+
+//    count++
+// }
+
+// }
+// console.log(count);
+
+
---
+++ new2.js
@@ -0,0 +1,55 @@
+// 1 ildizdan chiqarish
+// function testsqrt(son){
+//     return son ** 0.5
+// }
+// console.log(testsqrt(9));
+
+// 2 sonni darjaga ko'tarish
+// function testPow(son,daraja){
+//     return son ** daraja;
+// };
+// console.log(testPow(9, 2));
+
+// 3 musbat qaytarish
+// function testabs (son){
+//     return Math.abs(son);
+// };
+// console.log (testabs(-10));
+
+// 4 sonni yaxlitlash
+// function testround (son){
+//     return Math.round(son);
+// };
+// console.log (testround(12.6));
+
+// 5 sonni butun CountQueuingStrategymini qaytarish 
+// function testfloor (son){
+//     return Math.floor(son);
+// };
+// console.log (testfloor(7.9));
+
+// 6 sonni qoldiq qismiga son qoshish 
+// function testceil (son){
+//     return Math.ceil(son);
+// };
+// console.log(testceil(1995.7));
+
+// // 7 sonni butun yoki butun emasligini tekshiradi
+// function testislnteger (son){
+//     return Number.islnteger (son);
+// };
+// console.log (testislnteger(19.9));
+
+// 8 sonni faqat butun qismini qaytaradi
+// function testparselnt (son){
+//     return Number.parseInt(son);
+// };
+// console.log (testparselnt(7.9));
+
+// // 9 sonni yaxlitlaydi, va qoldiq qismidan xoxlagan qismini ajratib olishish
+// let son = 7.2335657
+// function testtoFixed (son){
+
+//     return son.toFixed (3);
+// };
+// console.log(testtoFixed(son));
